74fd91e28d55a6f6669dc82444025d94d2a4723e
jish4593k <143131599+jish4593k@users.noreply.github.com>
2023-12-18 22:15:13 +0530

    Update F.py

--- F/F.py
+++ F/F.py
@@ -38,10 +38,10 @@ def generate_torch_tensor():
 
 
 def display_seaborn_plot():
-    # Dummy Seaborn plot (replace with your specific use case)
+   
     sns.lineplot(x=range(10), y=range(10))
     plt.savefig('./seaborn_plot.png')
-    return send_file('./seaborn_plot.png', download_name="seaborn_plot.png")
+    return send_file('./ng', download_name="seng")
 
 if __name__ == "__main__":
     os.makedirs('./uploads', exist_ok=True)
